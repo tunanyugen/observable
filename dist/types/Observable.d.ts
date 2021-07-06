c473be490ee460d17a7a2ef5905fc6f98ed62ac6
Tuan <tunanyugen@gmail.com>
2021-07-06 16:56:47 +0700

    .

---
+++ dist/types/Observable.d.ts
@@ -0,0 +1,9 @@
+export default class Observable {
+    observables: Observable[];
+    private _callback;
+    getCallbackByRef: () => Function;
+    setCallback: (callback: Function) => void;
+    constructor(callback?: Function);
+    Add: (...observable: Observable[]) => Observable[];
+    Resolve: (parameter?: any) => any;
+}
