dad0afca9dd42a7ce324ffba03e7949af04fb967
Tuan <tunanyugen@gmail.com>
2021-07-13 21:07:14 +0700

    add Remove method, Dispose method, _parents, observables is now private

--- Observable.ts
+++ Observable.ts
@@ -1,5 +1,6 @@
 export default class Observable{
-    observables:Observable[] = [];
+    private _observables:Observable[] = [];
+    private _parents:Observable[] = [];
     private _callback:Function;
     getCallbackByRef = () => { return this._callback; }
     setCallback = (callback:Function) => { this._callback = callback; }
@@ -7,19 +8,37 @@ export default class Observable{
     constructor(callback:Function = null){
         this.setCallback(callback);
     }
-    Add = (...observable:Observable[]) => {
-        this.observables.push(...observable);
-        return observable;
+    Add = (...observables:Observable[]) => {
+        for (let i = 0; i < observables.length; i++){
+            if (observables[i] != this){
+                this._observables.push(observables[i]);
+            } else {
+                console.error("Cannot add observable to itself.");
+            }
+        }
+        return observables;
+    }
+    Remove = (observable:Observable) => {
+        let index = this._observables.indexOf(observable);
+        this._observables.splice(index, 1);
     }
     // execute all observables and callback
     Resolve = (parameter?:any) => {
         // resolve all observables' callbacks
-        for (let o = 0; o < this.observables.length; o++){
-            this.observables[o].Resolve(parameter);
+        for (let o = 0; o < this._observables.length; o++){
+            this._observables[o].Resolve(parameter);
         }
         // resolve self callback
         if (this._callback){
             return this._callback(parameter);
+        }
+    }
+    Dispose = () => {
+        for (let i = 0; i < this._observables.length; i++){
+            this._observables[i].Dispose();
+        }
+        for (let i = 0; i < this._parents.length; i++){
+            this._parents[i].Remove(this);
         }
     }
 }
