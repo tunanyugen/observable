a4926f6cf72163bdb210215a883743ecf2f9b0a1
Tuan Nguyen <tunanyugen@gmail.com>
2021-10-28 11:18:17 +0000

    - Removed test code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
 import Observable from "./ts/Observable";
-
-(window as any).Observable = Observable;
-let arg1 = "ADHJKASHKD";
-let arg2 = 1231232154;
-let a = new Observable<{a1:string, a2:number}>(()=>{}, false);
-let b = new Observable<{a1:string, a2:number}>((args) => {
-    console.log(args);
-}, false)
-a.AddObservable(b);
-a.Resolve({a1:arg1, a2:arg2});
 export default Observable;
