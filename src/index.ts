167996d0ab604345230e72d80a0b8d962e172c06
Tuna <tunanyugen@gmail.com>
2022-01-04 15:12:43 +0700

    - Removed console log

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 import Observable from "./ts/Observable";
 import { ObservableManager } from "./ts/ObservableManager";
 
-(window as any).Observable = Observable;
-
-let manager = new ObservableManager();
-let a = new Observable(manager, null, false);
-let b = new Observable(manager, () => {
-    console.log("ASD");
-}, false)
-a.AddObservable(b);
-console.log(manager, a);
-
 export default Observable;
